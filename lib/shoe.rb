e9b9294a70613c9d46685375f28a0d2168d65025
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-18 18:35:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,12 @@
 class Shoe 
+  def initialize(brand)
+    @brand = brand
+  end 
   
+  attr_accessor :brand 
+  attr_accessor :color 
+  attr_accessor :size 
+  attr_accessor :material 
+  attr_accessor :condition 
+  
+end 
